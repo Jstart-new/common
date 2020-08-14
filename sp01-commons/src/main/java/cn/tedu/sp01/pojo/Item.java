5a8ff2d34997344a26cb4414ffd4ca3941c5fd1d
jiangyuxiang <jiangyuxiang@LAPTOP-A6HS1RAA>
2020-08-14 14:56:53 +0800

    第三次修改

--- sp01-commons/src/main/java/cn/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/cn/tedu/sp01/pojo/Item.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Item {
 	 //��һ���޸�ID
-	// 第三次修改ID
+	//第三次修改ID
 	private Integer id;
 	private String name;
 	private Integer number;
